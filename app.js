6ac3bd2fa8a7afc1297b67a628331a6b43c6c7fa
lxykad <503215875@qq.com>
2017-01-23 13:50:13 +0800

    添加首页妹子

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 //app.js
 App({
+  //声明全局变量
+  data: {
+    gankBaseUrl:"http://gank.io/api/data"
+  },
   onLaunch: function () {
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
   },
-  getUserInfo:function(cb){
+  getUserInfo: function (cb) {
     var that = this
-    if(this.globalData.userInfo){
+    if (this.globalData.userInfo) {
       typeof cb == "function" && cb(this.globalData.userInfo)
-    }else{
+    } else {
       //调用登录接口
       wx.login({
         success: function () {
@@ -24,7 +28,7 @@ App({
       })
     }
   },
-  globalData:{
-    userInfo:null
+  globalData: {
+    userInfo: null
   }
 })
--- pages/meizi/meizi.js
+++ pages/meizi/meizi.js
@@ -1,1 +1,70 @@
 //index.js
+
+Page({
+    data: {
+        btnText: "bt1",
+        isShow: false,
+        news: [],
+    },
+
+    onLoad: function (options) {
+        // 生命周期函数--监听页面加载
+        var that = this
+
+        wx.request({
+            url: "https://gank.io/api/data/福利/10/1",
+            data: {},
+            method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+            header: { 'Accept': 'application/json' }, // 设置请求的 header
+            success: function (res) {
+                // success
+                console.log(res.data.results)
+                that.setData({
+                    news: res.data.results
+                })
+
+            },
+            fail: function () {
+                // fail
+            },
+            complete: function () {
+                // complete
+            }
+        })
+
+    },
+    onReady: function () {
+        // 生命周期函数--监听页面初次渲染完成
+
+    },
+    onShow: function () {
+        // 生命周期函数--监听页面显示
+
+
+    },
+    onHide: function () {
+        // 生命周期函数--监听页面隐藏
+
+    },
+    onUnload: function () {
+        // 生命周期函数--监听页面卸载
+
+    },
+    onPullDownRefresh: function () {
+        // 页面相关事件处理函数--监听用户下拉动作
+        console.log("===========refresh")
+    },
+    onReachBottom: function () {
+        // 页面上拉触底事件的处理函数
+        console.log("===========loadmore")
+    },
+    onShareAppMessage: function () {
+        // 用户点击右上角分享
+        return {
+            title: 'title', // 分享标题
+            desc: 'desc', // 分享描述
+            path: 'path' // 分享路径
+        }
+    }
+   
+})
